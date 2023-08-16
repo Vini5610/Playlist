ebf2a9801d438d971af684535b7dd5c9ea1726ba
Claudio Vinicius <79315178+Vini5610@users.noreply.github.com>
2023-08-16 11:58:35 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+window.audios = [
+  {
+    title:
+      "Lorem ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only ",
+    artist: "Rogério Skylab",
+    cover: "dolar.gif",
+    file: "Dolla.mp3"
+  },
+  {
+    title: "Júnior Pleno ou Sênior, qual a diferença?",
+    artist: "Diego Fernandes",
+    cover: "corte.jpg",
+    file: "corte america.mp3"
+  },
+  {
+    title: "Júnior Pleno ou Sênior, qual a diferença?",
+    artist: "Diego Fernandes",
+    cover: "corte.jpg",
+    file: "corte america.mp3"
+  },
+  {
+    title: "Júnior Pleno ou Sênior, qual a diferença?",
+    artist: "Diego Fernandes",
+    cover: "link-park.jpg",
+    file: "Linkin_Park_-_Numb_(BornMP3.com).mp3"
+  },
+  {
+    title: "Lorem ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only ",
+    artist: "Linkin Park",
+    cover: "x1.jpg",
+    file: "x1.mp3"
+  },
+
+];
